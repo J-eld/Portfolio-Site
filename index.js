d614f979ab23d3f8057c8475847f4692a7909017
J-eld <70251176+J-eld@users.noreply.github.com>
2021-02-26 15:12:18 +1300

    New content

--- index.js
+++
@@ -1,8 +0,0 @@
-var controller = new ScrollMagic.Controller();
-
-var HelloScene = new ScrollMagic.Scene({
-    triggerElement: '.hello',
-    
-})
-.setClassToggle('.hello', 'show')
-.addTo(controller)
