fbeb2823cdb41cd754dc757275989dbdb46a8e7a
J-eld <70251176+J-eld@users.noreply.github.com>
2021-02-27 13:52:27 +1300

    adding contact page functionality

---
+++ assets/js/contact.js
@@ -0,0 +1,10 @@
+function sendMail() {
+    console.log("Name = " + document.getElementById('name').value)
+    console.log("Email = " + document.getElementById('email').value)
+
+    
+   
+
+    document.getElementById('name').value = '';
+    document.getElementById('email').value = '';
+}
